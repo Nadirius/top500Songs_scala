a46128ffd6cc80d34d5e126a604afb27e99d8bb4
infinitWaves <104868269+infinitWaves@users.noreply.github.com>
2022-05-05 11:54:57 +0200

    mainApp -> start implementing spark

--- src/main/scala/AppMenu.scala
+++ src/main/scala/AppMenu.scala
@@ -2,16 +2,19 @@
 
 object AppMenu {
 
-  def displayMenu : () ⇒ Unit = () ⇒ println(
-    s"""
+  def displayMenu : () ⇒ Unit = () ⇒ {
+    println(
+      s"""
 MENU :
 \tpress 1 : To view top 500 songs ordered by position then by streak
 \tpress 2 : To view top 500 songs ordered by released date
 \tpress 3 : To view not classified songs
 \tpress 4 : To look for a songs by title
 \tpress 5 : To view Songs by writer
+\tpress 0 : To quit
 """)
-
+    print("Your choice : ")
+  }
 
 
 }
--- src/main/scala/Top500SongsObject.scala
+++ src/main/scala/Top500SongsObject.scala
@@ -8,9 +8,9 @@ object Top500SongsObject {
     def display(): Unit
   }
 
-  case class Song(title:String, description:String, appearsOn : String, artist: String, writers:String, producer:String, released:LocalDate)
+  case class Song(title:String, description:String, appearsOn : String, artist: String, writers:String, producer:String, released:String)
 
-  class Classified(override val title:String, override val description:String, override val appearsOn : String, override val artist: String, override val writers:String, override val producer:String, override val released:LocalDate, streak:String, position:String) extends Song(title:String, description:String, appearsOn : String, artist: String, writers:String, producer:String, released:LocalDate) with Displayable
+  class Classified(override val title:String, override val description:String, override val appearsOn : String, override val artist: String, override val writers:String, override val producer:String, override val released:String, streak:String, position:String) extends Song(title:String, description:String, appearsOn : String, artist: String, writers:String, producer:String, released:String) with Displayable
   {
     override def display() : Unit = println(
       s"""
@@ -21,11 +21,11 @@ TITLE : $title
     WRITERS : $writers
     PRODUCER : $producer
     RELEASED : $released
-    STREAK : $streak
-    POSITION : $position
+    STREAK : $streak weaks
+    POSITION : N° $position
     """)
   }
-  class UnClassified(override val title:String, override val description:String, override val appearsOn : String, override val artist: String, override val writers:String, override val producer:String, override val released:LocalDate,comment:String) extends Song(title:String, description:String, appearsOn : String, artist: String, writers:String, producer:String, released:LocalDate) with Displayable
+  class UnClassified(override val title:String, override val description:String, override val appearsOn : String, override val artist: String, override val writers:String, override val producer:String, override val released:String,comment:String) extends Song(title:String, description:String, appearsOn : String, artist: String, writers:String, producer:String, released:String) with Displayable
   {
     override def display() : Unit = println(
       s"""
